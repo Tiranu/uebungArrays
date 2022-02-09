7408be088e6f74fc0e0bdcbd708e74698b4f74ae
Tiranu <fricke.junior@googlemail.com>
2022-02-09 18:47:42 +0100

    Funktion renameFriday() und etwas smartere Version programmiert

--- uebung.js
+++ uebung.js
@@ -8,5 +8,21 @@ function ausgabe() {
     }
 }
 
+function renameFriday() {
+    wochentage[4] = "Nochnichtfreitag"
+    console.log("Aufgabe 4:");
+    ausgabe();
+}
+
+function renameFridaySmart() {
+    for (let i = 0; i < wochentage.length; i++) {
+        if(wochentage[i] === "Freitag")
+        {
+            wochentage[i] = "Nochnichtfreitag"
+        }
+    }
+    ausgabe();
+}
+
 
 
